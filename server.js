e50954ccac8da51c4d96393aa6d971907e61872c
daverowman <davewarc@gmail.com>
2025-02-24 15:35:31 +0800

    fix: serverless issue

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-import serverless from 'serverless-http';
 import routes from './src/routes/index.js'; // Ensure you use .js extension
 import './cronJobs.js';
 
@@ -23,5 +22,3 @@ if (process.env.NODE_ENV !== 'production') {
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
-
-export const handler = serverless(app);
