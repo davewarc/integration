fdeb06f435fef6b80ce2ca43dae587073b1a0fbc
daverowman <davewarc@gmail.com>
2025-02-27 10:52:56 -0600

    update cronjob test api endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,10 +14,32 @@ router.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-router.get('/run-cron-job', async (req, res) => {
+router.get('/run-cron-job/push-order', async (req, res) => {
   try {
     // Manually trigger the cron job's function
     await pushOrderFromBrightstoresToDeposco();
+    res.status(200).json({ message: 'Cron job executed successfully for debugging.' });
+  } catch (error) {
+    console.error('Error running cron job manually:', error);
+    res.status(500).json({ error: 'Error running cron job.' });
+  }
+});
+
+router.get('/run-cron-job/sync-newuser-points', async (req, res) => {
+  try {
+    // Manually trigger the cron job's function
+    await syncNewUsersPoints();
+    res.status(200).json({ message: 'Cron job executed successfully for debugging.' });
+  } catch (error) {
+    console.error('Error running cron job manually:', error);
+    res.status(500).json({ error: 'Error running cron job.' });
+  }
+});
+
+router.get('/run-cron-job/sync-everyusers-points', async (req, res) => {
+  try {
+    // Manually trigger the cron job's function
+    await syncGainsightPointsToBrightstores();
     res.status(200).json({ message: 'Cron job executed successfully for debugging.' });
   } catch (error) {
     console.error('Error running cron job manually:', error);
